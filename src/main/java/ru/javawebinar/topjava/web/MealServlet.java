22631476650650487b44a414f78c64e3837ab071
Maris Gabrans <maris_gabrans@yahoo.com>
2021-02-15 16:56:48 +0300

    HW01 in process. Missing JSP

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,17 +1,22 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.util.MealsUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 public class MealServlet extends HttpServlet {
-    private static final Logger LOGGER = null;
+    private static final Logger logger = LoggerFactory.getLogger(MealServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        logger.info("getAll");
+        req.setAttribute("meals", MealsUtil.getTos(MealsUtil.meals, MealsUtil.DEFAULT_CALORIES_PER_DAY));
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
     }
 }
